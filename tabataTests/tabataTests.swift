0fa481e246469cac6bf60e72035ef1c256a83273
Allen Russell <garyrusselljr@gmail.com>
2026-02-27 19:13:10 -0700

    Initial Commit

---
+++ tabataTests/tabataTests.swift
@@ -0,0 +1,16 @@
+//
+//  tabataTests.swift
+//  tabataTests
+//
+//  Created by Allen Russell on 2/27/26.
+//
+
+import Testing
+
+struct tabataTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
